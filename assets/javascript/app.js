482f3995108793ef860b377bd0153e7b60e813ac
AlCast <alvaro.castelan.21@gmail.com>
2018-04-26 20:48:00 -0500

    got values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,15 +13,34 @@ $(document).ready(function () {
         a.text("Start");
         // Adding the button to the buttons-view div
         $("#begin").append(a);
-
+        // hide questions until start has been pressed
         
     }
     renderStartButton()
+    $("#begin").on("click", function (event) {
+        event.preventDefault()
+        //show questions on click 
+        // get value of clicks 
+        var selValue1 = $("input[name=quest1]:checked").val();
+        console.log(selValue1)
+        var selValue2 = $("input[name=quest2]:checked").val();
+        console.log(selValue2)
+        var selValue3 = $("input[name=quest3]:checked").val();
+        console.log(selValue3)
+        var selValue4 = $("input[name=quest4]:checked").val();
+        console.log(selValue4)
+        var selValue5 = $("input[name=quest5]:checked").val();
+        console.log(selValue5)
+        var selValue6= $("input[name=quest6]:checked").val();
+        console.log(selValue6)
+        var selValue7 = $("input[name=quest7]:checked").val();
+        console.log(selValue7)
+        var selValue8 = $("input[name=quest8]:checked").val();
+        console.log(selValue8)
+        
 })
 
-//add on click event for start
-$("#begin").on("click", function (event) {
-    event.preventDefault()
+
 
 })
-
+ 
